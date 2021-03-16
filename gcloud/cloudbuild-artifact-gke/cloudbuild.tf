3526b957ccf017bfac5def1e3d49c3899f487a16
John Cheng <cheng.john@pm.me>
2021-03-16 15:03:57 +0200

    Add deployment to k8s

--- gcloud/cloudbuild-artifact-gke/cloudbuild.tf
+++ gcloud/cloudbuild-artifact-gke/cloudbuild.tf
@@ -27,24 +27,58 @@ resource "google_cloudbuild_trigger" "build-trigger" {
       args = [
         "build",
         "-t",
-        "gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA",
+        "gcr.io/$PROJECT_ID/${var.repository_name}:$COMMIT_SHA",
         ".",
       ]
     }
 
-    artifacts {
-      images = ["gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA"]
+    step {
+      id         = "repo-name-replacement"
+      name       = "gcr.io/cloud-builders/gcloud"
+      entrypoint = "/bin/sh"
+      args = ["-c",
+        "sed -i 's/CLUSTER_NAME/${var.repository_name}/g' ./kubernetes/deployment.yml",
+      ]
     }
 
-    # step {
-    #   name = "gcr.io/cloud-builder/gke-deploy"
-    #   args = [
-    #     "run", 
-    #     "--image=${var.gcr_region}.gcr.io/$PROJECT_ID/$REPO_NAME:$SHORT_SHA", 
-    #     "--location", "${var.location}", 
-    #     "--cluster", "${var.cluster_name}", 
-    #     "--expose", "80"
-    #   ]
-    # }
+    step {
+      id         = "project-id-replacement"
+      name       = "gcr.io/cloud-builders/gcloud"
+      entrypoint = "/bin/sh"
+      args = ["-c",
+        "sed -i 's/PROJECT_ID/$PROJECT_ID/g' ./kubernetes/deployment.yml",
+      ]
+    }
+    step {
+      id         = "commit-sha-replacement"
+      name       = "gcr.io/cloud-builders/gcloud"
+      entrypoint = "/bin/sh"
+      args = ["-c",
+        "sed -i 's/COMMIT_SHA/$COMMIT_SHA/g' ./kubernetes/deployment.yml",
+      ]
+      wait_for = ["project-id-replacement"]
+    }
+
+    step {
+      name = "gcr.io/cloud-builders/kubectl"
+      args = [
+        "apply",
+        "-f",
+        "./kubernetes/deployment.yml"
+      ]
+      env = [
+        "CLOUDSDK_COMPUTE_REGION=${var.region}",
+        "CLOUDSDK_CONTAINER_CLUSTER=${var.cluster_name}"
+      ]
+      wait_for = [
+        "repo-name-replacement",
+        "project-id-replacement",
+        "commit-sha-replacement",
+      ]
+    }
+
+    artifacts {
+      images = ["gcr.io/$PROJECT_ID/${var.repository_name}:$COMMIT_SHA"]
+    }
   }
 }
